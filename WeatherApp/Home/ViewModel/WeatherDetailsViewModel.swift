45055e68af722a42177d13a3bd5a0d3a298b2adb
Munsif Hayat <munsif.hayat@kodly.io>
2022-02-03 16:24:58 +0500

    Controllers, Models and ViewModels added, Network Manger Added, App Icon Added

---
+++ WeatherApp/Home/ViewModel/WeatherDetailsViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherDetailsViewModel.swift
+//  WeatherApp
+//
+//  Created by Munsif Hayat on 03/02/2022.
+//
+
+import Foundation
+
+struct WeatherDetailsViewModel {
+    
+    var weather_state_name: String
+    var weather_state_abbr: String
+    var the_temp : Double
+    var max_temp : Double
+    var min_temp : Double
+    var wind_speed: Double
+    var air_pressure : Double
+    var humidity : Int
+
+}
